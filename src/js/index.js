172c2224fec52196704d40b973f3a5106df71ac5
DidenkoSveta <garpia.lana@gmail.com>
2024-01-05 23:54:04 +0400

    mobile acordeon for shedule section

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,12 @@ import '../scss/style.scss'; // Путь к файлу стилей
 
 // Импорт функций из модулей
 import burgerMenu from './modules/burger-menu';
+import schedule from './modules/schedule';
 
 // Использование функций после полной загрузки DOM
 document.addEventListener('DOMContentLoaded', () => {
   burgerMenu();
+  schedule();
+
+
 });
